a3c0eac918efd251d1c477fa1936555e2e54ed37
panjiprann <panjipranantya20@gmail.com>
2025-10-23 09:24:24 +0700

    feat: tambahkan nama dan NIM di dalam Column

--- app/src/main/java/com/example/uipertama/MyUI.kt
+++ app/src/main/java/com/example/uipertama/MyUI.kt
@@ -50,4 +50,14 @@ fun AktivitasPertama(modifier: Modifier) {
                         .size(100.dp)
                         .padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+
 
