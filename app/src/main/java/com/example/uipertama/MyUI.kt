da476ecb6f31deffb50f7318896aa356b7fe6bd0
panjiprann <panjipranantya20@gmail.com>
2025-10-23 09:25:03 +0700

    feat: tambahkan teks alamat dengan resource string

--- app/src/main/java/com/example/uipertama/MyUI.kt
+++ app/src/main/java/com/example/uipertama/MyUI.kt
@@ -59,5 +59,16 @@ fun AktivitasPertama(modifier: Modifier) {
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 15.dp)
+
+                    )
+                }
+            }
+        }
+
 
 
