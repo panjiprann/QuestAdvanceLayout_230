a51b0b14f8d60483185f764d66431516132f9dc6
panjiprann <panjipranantya20@gmail.com>
2025-10-23 09:26:15 +0700

    style: perbesar ukuran font pada teks alamat

--- app/src/main/java/com/example/uipertama/MyUI.kt
+++ app/src/main/java/com/example/uipertama/MyUI.kt
@@ -61,7 +61,7 @@ fun AktivitasPertama(modifier: Modifier) {
                     )
                     Text(
                         text = stringResource(id = R.string.alamat),
-                        fontSize = 20.sp,
+                        fontSize = 21.sp,
                         color = Color.Yellow,
                         modifier = Modifier.padding(top = 15.dp)
 
