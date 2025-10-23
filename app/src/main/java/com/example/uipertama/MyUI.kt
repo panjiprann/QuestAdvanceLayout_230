256e915f1d841fe0b6277fdcd3c655bc393a7977
panjiprann <panjipranantya20@gmail.com>
2025-10-23 09:21:28 +0700

    tambahkan tampilan Column dengan teks judul prodi dan universitas

--- app/src/main/java/com/example/uipertama/MyUI.kt
+++ app/src/main/java/com/example/uipertama/MyUI.kt
@@ -22,4 +22,13 @@ fun AktivitasPertama(modifier: Modifier) {
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
