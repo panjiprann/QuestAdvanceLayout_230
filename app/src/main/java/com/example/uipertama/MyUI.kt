9dd856ae0376ccc151f62473122dfc2709a2e825
panjiprann <panjipranantya20@gmail.com>
2025-10-23 09:16:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uipertama/MyUI.kt
@@ -0,0 +1,16 @@
+package com.example.MyUI
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.uipertama.R
