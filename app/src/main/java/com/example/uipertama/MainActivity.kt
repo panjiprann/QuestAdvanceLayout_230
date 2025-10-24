8ab9e6637d1fd93802e4aaa304d7b7214d54e8d6
panjiprann <panjipranantya20@gmail.com>
2025-10-24 12:20:43 +0700

    refactor: panggil composable AktivitasPertama di MainActivity

--- app/src/main/java/com/example/uipertama/MainActivity.kt
+++ app/src/main/java/com/example/uipertama/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.MyUI.AktivitasPertama
 import com.example.uipertama.ui.theme.UIPertamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UIPertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AktivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
